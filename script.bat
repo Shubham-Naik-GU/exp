ac4847ea8d00fda243ac88be7cb83edeafeab6d5
SHUBHAM NAIK <144892678+Shubham-Naik-GU@users.noreply.github.com>
2024-10-24 12:15:27 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,12 +1,6 @@
 pipeline {
     agent any
     stages {
-        stage('Clean workspace and Navigate to the folder'){
-            steps{
-                cleanWs()
-                bat 'copy C:\\\\Users\\\\GBS\\\\Desktop\\\\java "C:\\Users\\GBS\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\piper"'
-            }
-        }
         stage('Build') {
             steps {
                 bat 'javac Hello.java'
